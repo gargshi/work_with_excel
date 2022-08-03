1ab83e85416eb7c29f760615e18084e114d98370
Shivam Garg <gargshivam34has@gmail.com>
2022-08-03 19:36:21 +0530

    added config file logic

--- feature.py
+++ feature.py
@@ -1,4 +1,23 @@
 import pandas
+import os
+
+
+def create_config_file(fn):
+	f=open(os.getcwd()+"\\"+fn,"w")
+	f.close()
+
+
+def config_file_create_if_not_exists():
+	fi="config.txt"
+	print(os.getcwd()+"\\"+fi,os.path.exists(os.getcwd()+"\\"+fi))
+	if os.path.exists(os.getcwd()+"\\"+fi):
+		print("config file present")
+	else:
+		print("config file not present")
+		create_config_file(fi)
+
+
 
 def add_to_file(fn,name,marks):
 	print("Adding record")
+	config_file_create_if_not_exists()
